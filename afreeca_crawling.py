48ad5b8fa0cc438d2a9623cbddfc67b0a35147e0
Seodongwan <sky121037@naver.com>
2020-01-06 11:55:57 +0900

    스크린샷 기능, 검색기능 추가

--- afreeca_crawling.py
+++ afreeca_crawling.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[105]:
+# In[1]:
 
 
 import requests
@@ -51,6 +51,7 @@ start_time_i = []
 category_i = []
 current_time_i = []
 bj_id_i = []
+prin_i = []
 write_wb.save('방송데이터.xlsx')
 rb = load_workbook(r"C:\Users\user\works\방송데이터.xlsx")
 
@@ -58,7 +59,7 @@ driver = webdriver.Chrome('C:/chromedriver.exe', options=options) #크롬 드라
 driver.get('http://www.afreecatv.com')
 
 
-# In[106]:
+# In[2]:
 
 
 def open_driver():
@@ -67,7 +68,7 @@ def open_driver():
     driver.get(driver.current_url)
 
 
-# In[107]:
+# In[3]:
 
 
 def save_bj_name():
@@ -76,17 +77,7 @@ def save_bj_name():
     write_ws['A{}'.format(rank)] = bj_name.text    
 
 
-# In[108]:
-
-
-def no_broad_cast():
-    info = driver.find_element_by_class_name("text_information").text
-# 데이터 수집 중 방송을 끌 경우 해당 방송 데이터 수집 안함
-    if "방송중이지 않습니다." in info:
-        driver.close()
-
-
-# In[109]:
+# In[4]:
 
 
 def save_title():
@@ -95,7 +86,7 @@ def save_title():
     write_ws['C{}'.format(rank)] = title.text
 
 
-# In[110]:
+# In[5]:
 
 
 def current_viewer():
@@ -103,10 +94,12 @@ def current_viewer():
     time.sleep(4)
     viewer = driver.find_element_by_xpath("""//*[@id="nAllViewer"]""")
     viewer_i.append(viewer.text)
+    if str(0) in viewer.text:
+        write_ws['D{}'.format(rank)] = '방송이 종료되었습니다'
     write_ws['D{}'.format(rank)] = viewer.text
 
 
-# In[111]:
+# In[6]:
 
 
 def save_start_time():
@@ -115,7 +108,14 @@ def save_start_time():
     write_ws['E{}'.format(rank)] = start_time.text
 
 
-# In[112]:
+# In[7]:
+
+
+def screenshot():
+    driver.save_screenshot("bj_{}.png".format(rank-1))
+
+
+# In[8]:
 
 
 def save_category():
@@ -124,7 +124,7 @@ def save_category():
     write_ws['F{}'.format(rank)] = category.text
 
 
-# In[113]:
+# In[9]:
 
 
 def bj_id():
@@ -133,7 +133,7 @@ def bj_id():
     write_ws['B{}'.format(rank)] = info_id[3]
 
 
-# In[114]:
+# In[10]:
 
 
 def current_time():
@@ -142,21 +142,30 @@ def current_time():
     write_ws['G{}'.format(rank)] = now_i
 
 
-# In[115]:
+# In[11]:
 
 
-while rank<8: 
+def prin():
+    aa = driver.find_element_by_xpath("""//*[@id="broadlist_area"]/div/ul/li[{}]/div/a[2]""".format(rank-1))
+    prin_i.append(aa.text)
+
+
+# In[12]:
+
+
+while rank<102:
     open_driver()
     save_bj_name()
-    no_broad_cast()
     bj_id()
     save_title()
     current_viewer()
     save_start_time()
     save_category()
+    screenshot()
     driver.close()
     current_time()
     driver.switch_to_window(driver.window_handles[0])
+    prin()
     rank += 1
     if not rank % 61:
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
@@ -164,10 +173,33 @@ while rank<8:
         time.sleep(3)
     if rank == 102:
         driver.quit()
-
-
 write_wb.save('방송데이터.xlsx')
 now = datetime.now()
 print("크롤링 끝난시간", now )
 print("\n진행시간 {}초".format(time.time() - stime))
+
+
+# In[24]:
+
+
+def search_i():
+    data = pd.read_excel('방송데이터.xlsx')
+    if search in prin_i:
+        indexno = prin_i.index(search)
+        print(data.loc[indexno])
+    else :
+        if search == 'quit':
+            print("종료")
+        else:
+            print("해당 bj가 존재하지 않습니다")
+
+
+# In[25]:
+
+
+while True:
+    search  = input("검색을 원하는 닉네임을 입력해주세요(quit 입력시 검색종료)")
+    search_i()
+    if search == 'quit':
+        break;
 
